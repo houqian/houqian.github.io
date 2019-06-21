c2da3b2bdeed5933154a7237726ea24ae6f6063d
侯乾 <houqian1991@foxmail.com>
2019-06-22 02:03:54 +0800

    update

--- js/mindmap.js
+++ js/mindmap.js
@@ -1,3 +1,3 @@
-// build time:Fri Jun 21 2019 20:32:58 GMT+0800 (GMT+08:00)
+// build time:Sat Jun 22 2019 02:03:52 GMT+0800 (GMT+08:00)
 $(document).ready(function(){$(".mindmap").each(function(){MM_FUNCS.drawMindMap(this)})});var MM_FUNCS={li2jsonData:function(i){var n;var t=i.children("a:first");if(t.length!==0){n={data:{text:t.text(),hyperlink:t.attr("href")}}}else{n={data:{text:i[0].childNodes[0].nodeValue.trim()}}}i.find("> ul > li").each(function(){if(!n.hasOwnProperty("children")){n.children=[]}n.children.push(MM_FUNCS.li2jsonData($(this)))});return n},drawMindMap:function(i){var n=$(i).find(">ul:first");var t={root:{}};var a=new kityminder.Minder({renderTo:i});t.root=MM_FUNCS.li2jsonData(n.children("li:first"));a.importData("json",JSON.stringify(t));a.disable();a.execCommand("hand");$(n).hide()}};
 //rebuild by neat 
