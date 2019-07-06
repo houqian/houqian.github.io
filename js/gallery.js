c2db364887bb7d21582164e9f0f80663850484d9
侯乾 <houqian1991@foxmail.com>
2019-07-06 22:44:17 +0800

    update

---
+++ js/gallery.js
@@ -0,0 +1,3 @@
+// build time:Sat Jul 06 2019 22:44:14 GMT+0800 (GMT+08:00)
+document.addEventListener("DOMContentLoaded",function(){if(typeof $.fn.lightGallery==="function"){$(".article").lightGallery({selector:".gallery-item"})}if(typeof $.fn.justifiedGallery==="function"){$(".justified-gallery").justifiedGallery()}});
+//rebuild by neat 
